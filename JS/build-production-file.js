4ce9d2c2720ed21ff0c0bf1ad8114986e0b1ff5a
Daniel Rieck <danielcrieck@gmail.com>
2016-06-07 10:42:48 -0700

    Debugging semantic error
    Correcting copy/paste errors.

--- JS/build-production-file.js
+++ JS/build-production-file.js
@@ -1,7 +1,7 @@
 /* NODE.JS - Build Production JavaScript File */
 var concat = require('../../../../AppData/Roaming/npm/node_modules/concat-files');
 concat([
- './sumres-custom.js',
+ './fye-custom.js',
  '../WSU-UE---JS/jQuery.oue-custom.js',
  '../WSU-UE---JS/jQuery.qTip.js',
  '../WSU-UE---JS/jQuery.masonry.js'
