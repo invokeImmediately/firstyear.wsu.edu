3b4388a113fe6e1adc0785bef1a58282cc49671a
Daniel Rieck <danielcrieck@gmail.com>
2016-06-12 23:12:37 -0700

    Fixing copy/paste error
    Corrected variable name that had been carried over from the DSP website and still needed to be changed.

--- JS/fye-custom.js
+++ JS/fye-custom.js
@@ -4,19 +4,19 @@
  *********************************************************************************************************************/
 (function ($) {
 	$(document).ready(function () {
-			/**********************************************************************************************
-			 * Tweak HTML source to work around some quirks of WordPress setup                            *
-			 **********************************************************************************************/
-			var dspSiteURL = window.location.pathname;
-			switch(dspSiteURL) {
+        /**********************************************************************************************
+         * Tweak HTML source to work around some quirks of WordPress setup                            *
+         **********************************************************************************************/
+        var fyeSiteURL = window.location.pathname;
+        switch(fyeSiteURL) {
 /*				case '/':
-					$('#menu-item-35').remove();
-					$('#spine-sitenav ul li').first().css('border-top', 'none');
-					$('#spine-sitenav').addClass('homeless');
-					break;*/
-				case '/news/':
-					$('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
-					break;
-			}
+                $('#menu-item-35').remove();
+                $('#spine-sitenav ul li').first().css('border-top', 'none');
+                $('#spine-sitenav').addClass('homeless');
+                break;*/
+            case '/news/':
+                $('div.column.one').first().parent('section').before('<section class="row single gutter pad-top"><div class="column one"><section class="article-header header-newsEvents"><div class="header-content"><h2>News</h2><h3>What We and Our Students Have Accomplished</h3></div></section></div></section>');
+                break;
+        }
 	});
 })(jQuery);
